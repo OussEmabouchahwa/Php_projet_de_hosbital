5af9d6e02e2cc6f9c8575fd805568abef414d6d4
Oussema <126197852+OussEmabouchahwa@users.noreply.github.com>
2024-05-12 22:23:37 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Bouchahwa Oussema | All Rights Reserved | Contact Us: +216 53 106 229</b></span></div>
+</footer>
+    
